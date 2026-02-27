a6cd0757a2e5962ec2c304adae58411dadf80254
DmitryAstapenko <dmitryastapenko1@gmail.com>
2026-02-27 17:57:09 +0300

    feat: add variants for input component

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -2,14 +2,27 @@ import React from 'react';
 import classNames from 'classnames';
 import styles from './Input.module.scss';
 
-export type InputSize = 'small' | 'default';
+export type InputVariant = 'default' | 'error' | 'success';
 
 export type InputProps = React.InputHTMLAttributes<HTMLInputElement> & {
-  elementSize?: InputSize;
+  large?: boolean;
+  variant?: InputVariant;
 };
 
-const Input = ({ className, elementSize = 'default', ...props }: InputProps) => {
-  return <input className={classNames(styles.input, styles[elementSize], className)} {...props} />;
+const Input = ({
+  className,
+  type = 'text',
+  variant = 'default',
+  large = false,
+  ...props
+}: InputProps) => {
+  return (
+    <input
+      type={type}
+      className={classNames(styles.input, styles[variant], { [styles.large]: large }, className)}
+      {...props}
+    />
+  );
 };
 
 Input.displayName = 'Input';
