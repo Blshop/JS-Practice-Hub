b050ecffaf055b00ed674d54cf6b6f130f03763d
DmitryAstapenko <dmitryastapenko1@gmail.com>
2026-02-27 13:58:05 +0300

    feat: implement text component

---
+++ src/components/Text/Text.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import classNames from 'classnames';
+import styles from './Text.module.scss';
+
+export type TextElement = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p' | 'label' | 'div' | 'span';
+
+export type TextProps = React.HTMLAttributes<HTMLElement> & {
+  tag?: TextElement;
+  muted?: boolean;
+  bold?: boolean;
+};
+
+const Text = ({ tag, className, children, muted, bold, ...props }: TextProps) => {
+  const Component = tag ?? 'div';
+
+  return (
+    <Component
+      className={classNames(
+        styles.text,
+        styles[Component],
+        { [styles.muted]: muted },
+        { [styles.bold]: bold },
+        className,
+      )}
+      {...props}
+    >
+      {children}
+    </Component>
+  );
+};
+
+Text.displayName = 'Text';
+export default Text;
