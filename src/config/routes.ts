34dab69ad4dca018aaace15ae9428f445692e796
Blshop <envworking@gmail.com>
2026-03-16 17:06:48 +0300

    feat: add routes fo quiz page

---
+++ src/config/routes.ts
@@ -0,0 +1,27 @@
+export const routes = {
+  main: {
+    mask: '/',
+    create: () => '/',
+  },
+  about: {
+    mask: '/about',
+    create: () => `/about`,
+  },
+  auth: {
+    mask: '/auth/*',
+    login: '/auth/login',
+    register: '/auth/register',
+    create: {
+      login: () => '/auth/login',
+      register: () => '/auth/register',
+    },
+  },
+  demo: {
+    mask: '/demo',
+    create: () => `/demo`,
+  },
+  quiz: {
+    mask: '/quiz',
+    create: () => `/quiz`,
+  },
+};
