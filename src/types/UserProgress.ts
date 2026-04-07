15aefd3f7df1df47f96786806a32046508021d59
DmitryAstapenko <dmitryastapenko1@gmail.com>
2026-04-07 10:51:30 +0300

    feat: add user progress type definition

---
+++ src/types/UserProgress.ts
@@ -0,0 +1,15 @@
+export interface QuestionStat {
+  readonly questionId: string;
+  readonly successCount: number;
+  readonly failedCount: number;
+}
+
+export interface LessonProgress {
+  readonly successAttempt: number;
+  readonly failedAttempt: number;
+  readonly questions: readonly QuestionStat[];
+}
+
+export interface UserServerProgress {
+  readonly lessons: Record<string, LessonProgress>;
+}
